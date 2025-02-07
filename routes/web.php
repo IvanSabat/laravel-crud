8ac48e368294e50ab5c46d7797970b3b5f90af1f
Ivan Sabat <ivan.sabat@outlook.com>
2025-02-07 11:27:08 +0200

    Add a controller for authorization

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::resource('/books', BookController::class)->except(['create', 'edit']);
